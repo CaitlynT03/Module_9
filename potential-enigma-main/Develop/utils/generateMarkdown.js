4eeb6ccebe7fe86bfa98e66b1315583a3ea977b5
CaitlynT03 <119358599+CaitlynT03@users.noreply.github.com>
2023-05-28 01:01:54 -0400

    Add files via upload

---
+++ potential-enigma-main/Develop/utils/generateMarkdown.js
@@ -0,0 +1,33 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+const renderLicenseBadge(license) => {
+  if (license === "none") {
+    return "";
+  } else {
+    return `![License](https://img.shields.io/badge/License-${license}-<blue>)`;
+  }
+};
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+const renderLicenseLink = (license) => {
+  if (license === "none") {
+    return "";
+  } else {
+    return `\n  * [License](#license) \n`
+  }
+};
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+const renderLicenseSection = (license) => {
+  if (license === "none") {
+    return "";
+  } else {
+    return `## License
+    
+  This app is covered under the ${license} license.`
+  }
+}
+
+module.exports = generateMarkdown;
